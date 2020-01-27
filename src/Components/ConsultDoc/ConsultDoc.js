d0b232962a577bee2a4ec2512607d1aeb680e5bf
Junaid Rahim <junaidrahim8d@gmail.com>
2020-01-27 18:17:00 +0530

    added router with 2 pages home and consultadoc

---
+++ src/Components/ConsultDoc/ConsultDoc.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ConsultDoc extends Component {
+    render(){
+        return (
+            <div>
+                <h1>I am Consult a Doc page</h1>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export class Home extends Component {
+
+    render(){
+        return(
+            <div>
+                <h1>I am home</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
